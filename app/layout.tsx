f739b36137176e75d643b0a98a9acd9afb9c31da
Aishete <trybunhengy@gmail.com>
2024-03-04 15:01:41 +0700

    update manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,20 @@ export const metadata = {
   title: "RW-Database",
   description: "A Database",
   manifest: "/manifest.json",
+  keywords: ["RW-Database"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Try bunheng" },
+    {
+      name: "Try bunheng",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 export default function RootLayout({
