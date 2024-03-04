9a2f725010eba7aeb46e20e30cf1130d335d5629
Aishete <trybunhengy@gmail.com>
2024-03-04 15:24:59 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,32 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true, // Enable SWC minification for improved performance
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-  },
-};
-const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true,
-  // Skip waiting for service worker activation
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "offlineCache",
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-  ],
-  buildExcludes: [/middleware-manifest.json$/],
-});
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-// Export the combined configuration for Next.js with PWA support
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+  },
+});
